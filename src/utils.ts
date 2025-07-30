d3a1aa6092654dcd0b24c19c29d1a06803101baf
Kadircan Bozkurt <kadircanbozkurt1905@gmail.com>
2025-07-30 14:14:44 +0300

    ref: fix default rewardAmount

--- src/utils.ts
+++ src/utils.ts
@@ -203,7 +203,7 @@ class GameState extends Struct({
   isSolved: Bool,
 }) {
   static default = new this({
-    rewardAmount: UInt64.from(1e9),
+    rewardAmount: UInt64.from(1e10),
     finalizeSlot: UInt32.from(0),
     lastPlayedSlot: UInt32.from(0),
     turnCount: UInt8.from(0),
